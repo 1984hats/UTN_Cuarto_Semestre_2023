f1fc900cefb02c70c8459f5f4bf6ac0889b4f483
Carlos Pereira <sillyhats.tv@gmail.com>
2023-09-24 03:03:54 -0300

    actualizo archivos del ecommerce
    actualizo archivos del ecommerce

--- Laboratorio IV/e-commerce2022/client/js/products.js
+++ Laboratorio IV/e-commerce2022/client/js/products.js
@@ -2,22 +2,43 @@ const productos = [
     {
         id: 1,
         productName: "Banana",
-        price: 480,
+        price: 10,
         quanty: 1,
         img: "/client/media/banana.png",
     },
     {
         id: 2,
         productName: "Leche",
-        price: 950,
+        price: 20,
         quanty: 1,
         img: "/client/media/leche.png",
     },
     {
         id: 3,
         productName: "Pollo",
-        price: 750,
+        price: 30,
         quanty: 1,
-        img: "/client/media/leche.png",
+        img: "/client/media/pollo.png",
     },
-]
+    {
+        id: 4,
+        productName: "Enlatados",
+        price: 40,
+        quanty: 1,
+        img: "/client/media/enlatados.png",
+    },
+    {
+        id: 5,
+        productName: "Mayonesa",
+        price: 50,
+        quanty: 1,
+        img: "/client/media/mayonesa.png",
+    },
+    {
+        id: 6,
+        productName: "Varios",
+        price: 60,
+        quanty: 1,
+        img: "/client/media/varios.png",
+    },
+];
