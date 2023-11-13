d9e219a3c93ecb89c93a8c7bbd2ad893a5f5ff36
Carlos Pereira <sillyhats.tv@gmail.com>
2023-11-13 19:39:57 -0300

    proyecto ecommerce deltabike front
    proyecto ecommerce deltabike front

---
+++ e-commerce_deltabike_2023/client/js/products.js
@@ -0,0 +1,58 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Huayra",
+        price: 523000,
+        quanty: 1,
+        img: "/client/media/img/bicicletas/01.jpg",
+    },
+    {
+        id: 2,
+        productName: "Toga",
+        price: 750000,
+        quanty: 1,
+        img: "/client/media/img/bicicletas/02.jpg",
+    },
+    {
+        id: 3,
+        productName: "Riva",
+        price: 323000,
+        quanty: 1,
+        img: "/client/media/img/bicicletas/03.jpg",
+    },
+    {
+        id: 4,
+        productName: "Arena 14",
+        price: 230000,
+        quanty: 1,
+        img: "/client/media/img/bicicletas/04.jpg",
+    },
+    {
+        id: 5,
+        productName: "Chopera",
+        price: 320000,
+        quanty: 1,
+        img: "/client/media/img/bicicletas/05.jpg",
+    },
+    {
+        id: 6,
+        productName: "Galapagos R26",
+        price: 260000,
+        quanty: 1,
+        img: "/client/media/img/bicicletas/06.jpg",
+    },
+    {
+        id: 6,
+        productName: "Performance",
+        price: 111111,
+        quanty: 1,
+        img: "/client/media/img/bicicletas/07.jpg",
+    },
+    {
+        id: 6,
+        productName: "Playera Dama Full",
+        price: 260000,
+        quanty: 1,
+        img: "/client/media/img/bicicletas/08.jpg",
+    },
+];
